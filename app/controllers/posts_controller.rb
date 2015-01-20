3c16d6269a3d1a02e737097feda2399b7e241f93
RossLitz <codingclasses.049@gmail.com>
2015-01-19 18:16:32 -0600

    Add comments to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
 	end
 
 	def show
+		@comments = Comment.where(post_id: @post)
 	end
 
 	def new
@@ -46,6 +47,6 @@ class PostsController < ApplicationController
 	end
 
 	def post_params
-		params.require(:post).permit(:title, :link, :description)
+		params.require(:post).permit(:title, :link, :description, :image, :name)
 	end
 end
